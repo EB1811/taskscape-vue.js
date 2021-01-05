2069dcf4993127058094651d826e3871fea91a95
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-01-05 09:36:33 +0000

    Created levels list.

---
+++ src/assets/Levels.ts
@@ -0,0 +1,7 @@
+export const Levels = [
+    {startExp: 0, endExp: 100},
+    {startExp: 101, endExp: 300},
+    {startExp: 301, endExp: 600},
+    {startExp: 601, endExp: 1000},
+    {startExp: 1001, endExp: 1500},
+]
