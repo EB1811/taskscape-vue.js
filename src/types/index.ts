33e4fa90dbb4d8da1aebe625f2d662eadd259612
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-02-02 11:59:52 +0000

    Added stat types and future possible types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,14 +7,21 @@ export interface Level {
     xpToNext: number;
 }
 // Format of Player Stats.
+//? Possibly graph stat history.
 export interface Stats {
     level: Level; // Total
     productivityL?: Level; // How much work done.
-    efficiencyL?: Level; // How quickly you do the work. In hours.
-    antiProcrastinationL?: Level; // Compare due date with done date.
+    efficiencyL?: Level; // How quickly you do the work.
+    antiProcrastinationL?: Level; // When you finish the work in relation to the due date.
+    predictabilityL?: Level; // How close your estimated time to complete is to the actual time it took.
+    hardWorkerL?: Level; // Do low difficulty but time consuming task. OR just time consuming tasks.
+    smartWorkerL?: Level; // Do high difficulty but quick task. OR just difficult tasks.
+    prioritizationL?: Level; // How well you prioritize important work. When you finish important tasks while having unimportant tasks to do.
+    //? More ideas: Focus - looking at type of tasks, Throughput - how much work (productivity) / how much time (efficiency), questins - looking at how much you complete quests,
 }
 
 // Format of a task.
+//TODO Add importance and due date.
 export interface Task {
     id?: string;
     name: string;
